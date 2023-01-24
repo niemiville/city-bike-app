4b5dcd9ae8c148dea431af1ae6b076d39a9a3d57
Ville Niemi <ville.m.niemi@tuni.fi>
2023-01-24 05:16:51 +0200

    added connecting to database

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,43 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
 const app = express();
-app.use(express.json());
+dotenv.config(); 
 
-const PORT = 3000;
-
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+const pool = new Pool({
+  connectionString: process.env.DB_URI
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.get("/test", (_req: Request, res: Response) => {
+  res.send("hi");
+});
+
+app.get("/test2", async (_req: Request, res: Response) => {
+  try {
+    const client = await pool.connect();
+
+    const sql = "SELECT * FROM todo";
+    const { rows } = await client.query(sql);
+    const todos = rows;
+
+    client.release();
+
+    res.send(todos);
+} catch (error) {
+    res.status(400).send(error);
+}
+});
+
+app.listen(process.env.PORT, () => {
+  void connectToDB();
+  console.log(`Database server is running on port ${process.env.PORT}`);
 });
