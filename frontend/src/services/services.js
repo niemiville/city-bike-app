933cad10aa9d3383695f47fc84d7315ed346719a
Ville Niemi <vilsuxn@gmail.com>
2023-02-04 22:24:07 +0200

    added station functions to services

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -1,14 +1,21 @@
-import axios from 'axios'
-const apiUrl = 'http://localhost:4000/journeys/'
-
-const getJourneys = async () => {
-  const journeys = await axios.get(apiUrl);
-  return journeys.data;
-}
+import axios from 'axios';
+const journeysUrl = 'http://localhost:4000/api/journeys/';
+const stationsUrl = 'http://localhost:4000/api/stations/';
+const singleStationUrl = 'http://localhost:4000/api/stations/singleview/';
 
 const getJourneysPage = async (page) => {
-  const journeys = await axios.get(apiUrl + page);
+  const journeys = await axios.get(journeysUrl + page);
   return journeys.data;
 }
 
-export { getJourneys, getJourneysPage };
+const getStationsPage = async (page) => {
+  const stations = await axios.get(stationsUrl + page);
+  return stations.data;
+}
+
+const getSingleStation = async (id) => {
+  const station = await axios.get(singleStationUrl + id);
+  return station.data;
+}
+
+export { getJourneysPage, getStationsPage, getSingleStation };
